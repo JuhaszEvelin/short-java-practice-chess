7fb423171a33c510193dcc7fd213302cf87f7b03
MiklosHorvath <horvathmikloswork@gmail.com>
2021-04-12 20:39:33 +0200

    implement Position class

--- src/main/java/com/codecool/chess/Position.java
+++ src/main/java/com/codecool/chess/Position.java
@@ -1,4 +1,27 @@
 package com.codecool.chess;
 
 public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isOnBoard(){
+        return x < Board.getSIZE() && y < Board.getSIZE();
+    }
+
+    public Position calculatePositionAfterMove(Movement movement){
+        return new Position(x + movement.getDeltaX(), y + movement.getDeltaY());
+    }
 }
