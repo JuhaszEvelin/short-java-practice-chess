c2d56825e36e613fe6cca353794ae90e30ecd53d
MiklosHorvath <horvathmikloswork@gmail.com>
2021-04-12 20:28:01 +0200

    implements movement class

--- src/main/java/com/codecool/chess/Movement.java
+++ src/main/java/com/codecool/chess/Movement.java
@@ -1,4 +1,19 @@
 package com.codecool.chess;
 
 public class Movement {
+    private int deltaX;
+    private int deltaY;
+
+    public Movement(int deltaX, int deltaY) {
+        this.deltaX = deltaX;
+        this.deltaY = deltaY;
+    }
+
+    public int getDeltaX() {
+        return deltaX;
+    }
+
+    public int getDeltaY() {
+        return deltaY;
+    }
 }
